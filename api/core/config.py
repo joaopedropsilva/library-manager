325d2bdd2a02391935762d8abf7b15ab8af0b9be
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-11-27 06:03:46 -0300

    Revert "fixed settings temporarily"
    This reverts commit 36e74b37536eb3402d21d7fed007e5dd1ecbc102.

--- api/core/config.py
+++ api/core/config.py
@@ -2,11 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    _postgres_host: str
-    _postgres_port: int
-    _postgres_user: int
-    _postgres_db: str
-    _postgres_password: str
     database_url: str
 
     model_config = {"env_file": ".env"}
