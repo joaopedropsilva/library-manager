36e74b37536eb3402d21d7fed007e5dd1ecbc102
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-11-27 01:33:05 -0300

    fixed settings temporarily

--- api/core/config.py
+++ api/core/config.py
@@ -2,6 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    _postgres_host: str
+    _postgres_port: int
+    _postgres_user: int
+    _postgres_db: str
+    _postgres_password: str
     database_url: str
 
     model_config = {"env_file": ".env"}
